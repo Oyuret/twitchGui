fd291abc923554e9054040f791f667ed00b9886d
Yuri Stange <yuris@kth.se>
2016-04-01 17:21:50 +0200

    Refactored kodiApi a bit

--- client/app/kodi/kodiAPI.factory.js
+++ client/app/kodi/kodiAPI.factory.js
@@ -45,32 +45,75 @@
 
     function pushPlay(stream, kodiAddress, deferred) {
 
-      var getPlayerId = {'jsonrpc': '2.0', 'method': 'Player.GetActivePlayers', 'id': 1};
-      var clearVideoPlaylist = {'jsonrpc': '2.0', 'method': 'Playlist.Clear', 'params':{'playlistid':1}, 'id': 1};
-      var pluginPath = 'plugin://plugin.video.twitch/playLive/' + stream + '/';
-      var addPlayList = [{'jsonrpc': '2.0', 'method': 'Playlist.Add', 'params':{'playlistid':1, 'item' :{ 'file' : pluginPath }}, 'id' :1}];
-      var playVideo = {'jsonrpc': '2.0', 'method': 'Player.Open', 'params':{'item':{'playlistid':1, 'position' : 0}}, 'id': 1};
-
-      $http.post('/kodi', {query: getPlayerId, kodi: kodiAddress}, {timeout:10000})
-        .then(function(idData){
-          if(idData.data.result.length === 0) {
-            return $http.post('/kodi', {query: clearVideoPlaylist, kodi: kodiAddress});
-          } else {
-            var id = idData.data.result[0].playerid;
-            var stopPlayer = {'jsonrpc': '2.0', 'method': 'Player.Stop', 'params': { 'playerid': id }, 'id': 1};
-            return $http.post('/kodi', {query: stopPlayer, kodi: kodiAddress});
-          }
+      getPlayerId(kodiAddress)
+        .then(function(getPlayerIdResponse){
+            return stopPlayerIfRunning(getPlayerIdResponse, kodiAddress);
         }).then(function(){
-          return $http.post('/kodi', {query: clearVideoPlaylist, kodi: kodiAddress}, {timeout:10000});
+          return clearVideoPlaylist(kodiAddress);
         }).then(function(){
-          return $http.post('/kodi', {query: addPlayList, kodi: kodiAddress}, {timeout:10000});
+          return addToPlaylist(stream, kodiAddress);
         }).then(function(){
-          return $http.post('/kodi',{query: playVideo, kodi: kodiAddress}, {timeout:30000});
+          return playVideo(kodiAddress);
         }).then(function(){
           deferred.resolve();
-        }, function(errormsg){
-          deferred.reject(angular.toJson(errormsg, true));
+        }, function(errorMsg){
+          deferred.reject(errorMsg);
         });
+    }
+
+    function getPlayerId(kodiAddress) {
+      var getPlayerIdRequestData = {'jsonrpc': '2.0', 'method': 'Player.GetActivePlayers', 'id': 1};
+      return $http.post('/kodi', {query: getPlayerIdRequestData, kodi: kodiAddress}, {timeout:10000});
+    }
+
+    function stopPlayerIfRunning(playerIdResponse, kodiAddress) {
+
+      if(playerIsRunning(playerIdResponse)) {
+        var id = playerIdResponse.data.result[0].playerid;
+        var stopPlayerRequestData = {'jsonrpc': '2.0', 'method': 'Player.Stop', 'params': { 'playerid': id }, 'id': 1};
+
+        return $http.post('/kodi', {query: stopPlayerRequestData, kodi: kodiAddress});
+      } else {
+        return $q.resolve();
+      }
+    }
+
+    function playerIsRunning(playerIdResponse) {
+      return playerIdResponse.data.result.length !== 0;
+    }
+
+    function clearVideoPlaylist(kodiAddress) {
+      var clearVideoPlaylistRequestData = {
+        'jsonrpc': '2.0',
+        'method': 'Playlist.Clear',
+        'params':{'playlistid':1},
+        'id': 1
+      };
+      return $http.post('/kodi', {query: clearVideoPlaylistRequestData, kodi: kodiAddress}, {timeout:10000});
+    }
+
+    function addToPlaylist(stream, kodiAddress) {
+      var pluginPath = 'plugin://plugin.video.twitch/playLive/' + stream + '/';
+      var addToPlaylistRequestData = [{
+        'jsonrpc': '2.0',
+        'method': 'Playlist.Add',
+        'params':{'playlistid':1, 'item' :{ 'file' : pluginPath }},
+        'id' :1
+      }];
+
+      return $http.post('/kodi', {query: addToPlaylistRequestData, kodi: kodiAddress}, {timeout:10000});
+    }
+
+    function playVideo(kodiAddress) {
+
+      var playVideoRequestData = {
+        'jsonrpc': '2.0',
+        'method': 'Player.Open',
+        'params':{'item':{'playlistid':1, 'position' : 0}},
+        'id': 1
+      };
+
+      return $http.post('/kodi',{query: playVideoRequestData, kodi: kodiAddress}, {timeout:30000});
     }
 
   }
