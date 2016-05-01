9f730076dcb6c3feade5853b2fbbb5183654e1d3
Yuri Stange <yuris@kth.se>
2016-05-01 21:34:46 +0200

    Simplified Kodi API in order to fix bug when rapidly changing streams

--- client/app/kodi/kodiAPI.factory.js
+++ client/app/kodi/kodiAPI.factory.js
@@ -45,76 +45,21 @@
 
     function pushPlay(stream, kodiAddress, playbackQuality, deferred) {
 
-      getPlayerId(kodiAddress)
-        .then(function(getPlayerIdResponse){
-          return stopPlayerIfRunning(getPlayerIdResponse, kodiAddress);
-        }).then(function(){
-          return clearVideoPlaylist(kodiAddress);
-        }).then(function(){
-          return addToPlaylist(stream, playbackQuality, kodiAddress);
-        }).then(function(){
-          return playVideo(kodiAddress);
-        }).then(function(){
-          deferred.resolve();
-        }).catch(function(errorMsg){
-          deferred.reject(errorMsg);
-        });
-    }
-
-    function getPlayerId(kodiAddress) {
-      var getPlayerIdRequestData = {'jsonrpc': '2.0', 'method': 'Player.GetActivePlayers', 'id': 1};
-      return $http.post('/api/kodi', {query: getPlayerIdRequestData, kodi: kodiAddress}, {timeout:10000});
-    }
-
-    function stopPlayerIfRunning(playerIdResponse, kodiAddress) {
-
-      if(playerIsRunning(playerIdResponse)) {
-        var id = playerIdResponse.data.result[0].playerid;
-        var stopPlayerRequestData = {'jsonrpc': '2.0', 'method': 'Player.Stop', 'params': { 'playerid': id }, 'id': 1};
-
-        return $http.post('/api/kodi', {query: stopPlayerRequestData, kodi: kodiAddress});
-      } else {
-        return $q.resolve();
-      }
-    }
-
-    function playerIsRunning(playerIdResponse) {
-      return playerIdResponse.data.result.length !== 0;
-    }
-
-    function clearVideoPlaylist(kodiAddress) {
-      var clearVideoPlaylistRequestData = {
-        'jsonrpc': '2.0',
-        'method': 'Playlist.Clear',
-        'params':{'playlistid':1},
-        'id': 1
-      };
-      return $http.post('/api/kodi', {query: clearVideoPlaylistRequestData, kodi: kodiAddress}, {timeout:10000});
-    }
-
-    function addToPlaylist(stream,  playbackQuality, kodiAddress) {
-      var pluginPath = `plugin://plugin.video.twitch/playLive/${stream}/${playbackQuality}`;
-      var addToPlaylistRequestData = [{
-        'jsonrpc': '2.0',
-        'method': 'Playlist.Add',
-        'params':{'playlistid':1, 'item' :{ 'file' : pluginPath }},
-        'id' :1
-      }];
-
-      return $http.post('/api/kodi', {query: addToPlaylistRequestData, kodi: kodiAddress}, {timeout:10000});
-    }
-
-    function playVideo(kodiAddress) {
-
+      var playStreamPath = `plugin://plugin.video.twitch/playLive/${stream}/${playbackQuality}`;
       var playVideoRequestData = {
         'jsonrpc': '2.0',
         'method': 'Player.Open',
-        'params':{'item':{'playlistid':1, 'position' : 0}},
+        'params':{'item':{'file' : playStreamPath }},
         'id': 1
       };
 
-      return $http.post('/api/kodi',{query: playVideoRequestData, kodi: kodiAddress}, {timeout:30000});
+      $http.post('/api/kodi',{query: playVideoRequestData, kodi: kodiAddress}, {timeout:30000})
+        .then(function(){
+          deferred.resolve();
+        })
+        .catch(function(errorMsg){
+          deferred.reject(errorMsg);
+        });
     }
-
   }
 })();
