576af1ff58d9791ed595f36298b8a69ed0334575
Yuri Stange <yuris@kth.se>
2016-08-12 14:33:34 +0200

    Added a back to top button

--- client/app/app.js
+++ client/app/app.js
@@ -8,7 +8,8 @@ angular.module('twitchGuiApp', [
   'ui.router',
   'ui.bootstrap',
   'cgBusy',
-  'angular.filter'
+  'angular.filter',
+  'angular.backtop'
 ])
   .config(function($urlRouterProvider, $locationProvider) {
     $urlRouterProvider
